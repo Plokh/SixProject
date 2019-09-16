0250b8cdc5175fa59a139c24f5a33bf20428d0c0
Plokh <31943464+Plokh@users.noreply.github.com>
2019-09-15 23:53:02 -0300

    Level Design
    Assets do mapa

---
+++ Assets/Scripts/ZombieAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieAnimation : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    float horizontal;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+
+
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        Movimentar(horizontal);
+    }
+
+    private void Movimentar(float h)
+    {
+        rb2D.velocity = new Vector2(h, rb2D.velocity.y);
+    }
+}
