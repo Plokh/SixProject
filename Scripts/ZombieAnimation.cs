9e1f195512dd643ea29be136545348c41e7219e9
Plokh <31943464+Plokh@users.noreply.github.com>
2019-09-17 17:56:10 -0300

    Level Design
    Adicionando componentes e inimigos ao level design

---
+++ Scripts/ZombieAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieAnimation : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    [SerializeField]
+    private float velocidade = 0;
+    float horizontal;
+    float vertical;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+
+
+    }
+
+    void Update()
+    {
+        vertical = Input.GetAxis("Vertical");
+        MovimentarY(vertical);
+
+        horizontal = Input.GetAxis("Horizontal");
+        MovimentarX(horizontal);
+
+    }
+
+        private void MovimentarY(float v)
+    {
+        rb2D.velocity = new Vector2(v*velocidade, rb2D.velocity.x);
+    }
+        private void MovimentarX(float h)
+    {
+        rb2D.velocity = new Vector2(h*velocidade, rb2D.velocity.x);
+    }
+}
