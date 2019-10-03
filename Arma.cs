8d58b8b06e727b58e4e375cf6a6fd06bfde1af51
Plokh <31943464+Plokh@users.noreply.github.com>
2019-10-02 22:29:30 -0300

    Projeto com as correções para a devolutiva
    Projeto com as correções para a devolutiva

---
+++ Arma.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Arma
+{
+    public GameObject armaPrefab;
+    public GameObject projetilPrefab;
+    public Transform projetilSaida;
+    public int dano = 12;
+    public float projetilVelocidade = 1f;
+
+    public Arma(GameObject arma)
+    {
+        _animator = arma.GetComponent<Animator>();
+        _transform = arma.GetComponent<Transform>();
+        _spriteRenderer = arma.GetComponent<SpriteRenderer>();
+    }
+
+    void Atirar()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            /*GameObject projetil = (GameObject)Instantiate(projetilPrefab, projetilSaida.position, projetilSaida.rotation); //Estancia objeto
+            projetil.GetComponent<Rigidbody2D>().velocity = direcao.normalized * projetilVelocidade; //joga o objeto*/
+        }
+    }
+
+}
